211d070f5e6e76ab26613450f77c1e95eac809e4
Excel Asaph <151797298+excel-asaph@users.noreply.github.com>
2024-12-07 00:56:33 +0200

    Update rewrite.py

--- rewrite.py
+++ rewrite.py
@@ -1,3 +1,24 @@
+PS C:\Users\Excel\Desktop\Python> python3 .\rewrite.py
+Page 1:
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor    
+incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore      
+eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in 
+culpa qui officia deserunt mollit anim id est laborum.
+
+Total Pages: 1
+Payment Due: $30
+
+
+
+
+
+
+
+
+
+
 import re
 
 class ArticleManager:
